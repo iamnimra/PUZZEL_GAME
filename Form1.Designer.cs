4988931b69cdfdfa7c7ae3a7d3394a972bb11dec
Nimra Naseem <95517297+iamnimra@users.noreply.github.com>
2021-12-06 11:37:46 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace SimplePuzzleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl = new System.Windows.Forms.Panel();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblNoOfMoves = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.pnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.Controls.Add(this.button16);
+            this.pnl.Controls.Add(this.button15);
+            this.pnl.Controls.Add(this.button14);
+            this.pnl.Controls.Add(this.button13);
+            this.pnl.Controls.Add(this.button12);
+            this.pnl.Controls.Add(this.button11);
+            this.pnl.Controls.Add(this.button10);
+            this.pnl.Controls.Add(this.button9);
+            this.pnl.Controls.Add(this.button8);
+            this.pnl.Controls.Add(this.button7);
+            this.pnl.Controls.Add(this.button6);
+            this.pnl.Controls.Add(this.button5);
+            this.pnl.Controls.Add(this.button4);
+            this.pnl.Controls.Add(this.button3);
+            this.pnl.Controls.Add(this.button2);
+            this.pnl.Controls.Add(this.button1);
+            this.pnl.Location = new System.Drawing.Point(13, 13);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(295, 309);
+            this.pnl.TabIndex = 0;
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.YellowGreen;
+            this.button16.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button16.Location = new System.Drawing.Point(184, 207);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(50, 50);
+            this.button16.TabIndex = 15;
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.YellowGreen;
+            this.button15.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button15.Location = new System.Drawing.Point(128, 207);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(50, 50);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "15";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.YellowGreen;
+            this.button14.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.Location = new System.Drawing.Point(72, 207);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(50, 50);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "14";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.YellowGreen;
+            this.button13.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(16, 207);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(50, 50);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "13";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.YellowGreen;
+            this.button12.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(184, 151);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(50, 50);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "12";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.YellowGreen;
+            this.button11.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(128, 151);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(50, 50);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "11";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.YellowGreen;
+            this.button10.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(72, 151);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(50, 50);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "10";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.YellowGreen;
+            this.button9.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(16, 151);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.YellowGreen;
+            this.button8.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(184, 95);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.YellowGreen;
+            this.button7.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(128, 95);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.YellowGreen;
+            this.button6.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(72, 95);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.YellowGreen;
+            this.button5.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(16, 95);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.YellowGreen;
+            this.button4.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(184, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.YellowGreen;
+            this.button3.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(128, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.YellowGreen;
+            this.button2.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(72, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.YellowGreen;
+            this.button1.Font = new System.Drawing.Font("Bell MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // lblNoOfMoves
+            // 
+            this.lblNoOfMoves.AutoSize = true;
+            this.lblNoOfMoves.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.lblNoOfMoves.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNoOfMoves.Font = new System.Drawing.Font("Bell MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNoOfMoves.Location = new System.Drawing.Point(469, 61);
+            this.lblNoOfMoves.Name = "lblNoOfMoves";
+            this.lblNoOfMoves.Size = new System.Drawing.Size(142, 21);
+            this.lblNoOfMoves.TabIndex = 1;
+            this.lblNoOfMoves.Text = "Number Of Moves";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnNewGame.Font = new System.Drawing.Font("Bell MT", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.Location = new System.Drawing.Point(469, 108);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(142, 50);
+            this.btnNewGame.TabIndex = 2;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(616, 359);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblNoOfMoves);
+            this.Controls.Add(this.pnl);
+            this.Name = "Form1";
+            this.Text = "SimplaPuzzleGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblNoOfMoves;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplePuzzleGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int movesNumber = 0, labelIndex = 0;
+        private void shuffleButtons()
+        {
+            List<int> labelList = new List<int>();
+            Random rand = new Random();
+
+            foreach (Button btn in this.pnl.Controls)
+            {
+                while (labelList.Contains(labelIndex))
+                    labelIndex = rand.Next(16);
+
+                btn.Text = (labelIndex == 0) ? "" : labelIndex + "";
+                btn.BackColor = (btn.Text == "") ? Color.White : Color.FromKnownColor(KnownColor.ControlLight);
+                labelList.Add(labelIndex);
+            }
+            movesNumber = 0;
+            lblNoOfMoves.Text = "Number of Moves :" + movesNumber;
+        }
+
+        private void swapLabel(Object sender,EventArgs e )
+        {
+            Button btn = (Button)sender;
+            if (btn.Text == "")
+                return;
+           Button whitebtn = null;
+            foreach(Button bt in this.pnl.Controls)
+            {
+                if(bt.Text == "")
+                {
+                    whitebtn = bt;
+                    break;
+                }
+
+            }
+            if(btn.TabIndex == (whitebtn.TabIndex -1)||
+                btn.TabIndex == (whitebtn.TabIndex - 4)||
+                btn.TabIndex == (whitebtn.TabIndex + 1)||
+                btn.TabIndex == (whitebtn.TabIndex + 4))
+            {
+                whitebtn.BackColor = Color.FromKnownColor(KnownColor.ControlLight);
+                btn.BackColor = Color.White;
+                whitebtn.Text = btn.Text;
+                btn.Text = "";
+                movesNumber++;
+                lblNoOfMoves.Text = "Number of Moves :" + movesNumber;
+            }
+            checkOrder();
+          }
+
+        private void checkOrder()
+        {
+            int index = 0;
+            foreach (Button btn in this.pnl.Controls)
+            {
+                if(btn.Text!="" && Convert.ToInt16(btn.Text)!=index)
+                {
+                    return;
+                }
+                index++;
+            }
+            MessageBox.Show("Congratulations!!! You did it in" + movesNumber +" moves");
+
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            shuffleButtons();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            shuffleButtons();
+        }
+    }
+}
